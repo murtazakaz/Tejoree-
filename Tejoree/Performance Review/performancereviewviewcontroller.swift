4288b9430428707c246415bb9c61cbb54956abf8
macbook <syed.murtazakaz@gmail.com>
2018-05-07 03:27:48 +0500

    h done

--- Tejoree/Performance Review/performancereviewviewcontroller.swift
+++ Tejoree/Performance Review/performancereviewviewcontroller.swift
@@ -1,53 +1,29 @@
 //
-//  AdditionDeletionViewController.swift
+//  performancereviewviewcontroller.swift
 //  Tejoree
 //
-//  Created by Macbook on 12/10/17.
-//  Copyright © 2017 Macbook. All rights reserved.
+//  Created by Macbook on 5/7/18.
+//  Copyright © 2018 Macbook. All rights reserved.
 //
-
 import UIKit
 
-class AdditionDeletionViewController: UIViewController ,UITableViewDelegate , UITableViewDataSource,UIPickerViewDataSource , UIPickerViewDelegate{
- 
+class performancereviewviewcontroller : UIViewController ,UIPickerViewDataSource , UIPickerViewDelegate{
+    
     @IBOutlet weak var dropdown: UIPickerView!
     @IBOutlet weak var dropdowntext: UILabel!
     
     var pagelist = ["HCP","Addition Deletion","Performance Review"]
+
     override func viewDidLoad() {
         super.viewDidLoad()
         dropdown.isHidden = true
-        dropdowntext.text = "Addition Deletion"
+        dropdowntext.text = "Performance Review"
         // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-        let cell = Bundle.main.loadNibNamed("AdditionDeletionTableViewCell", owner: self, options: nil)?.first as! AdditionDeletionTableViewCell
-       
-        
-        return cell
-        
-        
-        
-    }
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        
-        return 40
-        
-    }
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-       // let detailnews = storyboard?.instantiateViewController(withIdentifier: "PerformanceReviewPage")
-        //navigationController?.pushViewController( detailnews!, animated: true)
     }
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
@@ -66,18 +42,17 @@ class AdditionDeletionViewController: UIViewController ,UITableViewDelegate , UI
             navigationController?.pushViewController( highconvictionpage!, animated: true)
         }
         if(dropdowntext.text == "Addition Deletion"){
-          
+            let additionpage = storyboard?.instantiateViewController(withIdentifier: "addition")
+            navigationController?.pushViewController( additionpage!, animated: true)
         }
         if(dropdowntext.text == "Performance Review"){
-            let PerformanceReviewPage = storyboard?.instantiateViewController(withIdentifier: "PerformanceReviewPage")
-            navigationController?.pushViewController( PerformanceReviewPage!, animated: true)
+           
         }
         
         
     }
     @IBAction func dropdownbtn(_ sender: Any) {
-          dropdown.isHidden = false;
+        dropdown.isHidden = false;
         
     }
-    
 }
